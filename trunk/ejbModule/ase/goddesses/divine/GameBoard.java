1bd68c5902c6346102d609091059ea0a80a564c2
talklittle <talklittle@cd6f5ea5-f733-c8f2-e759-6d7a7f71d7d3>
2010-10-25 19:16:05 +0000

    ejbModule: some SessionBeans and stuff, src dir separate.

---
+++ trunk/ejbModule/ase/goddesses/divine/GameBoard.java
@@ -0,0 +1,20 @@
+package ase.goddesses.divine;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+
+/**
+ * Session Bean implementation class GameBoard
+ */
+@Stateful
+@LocalBean
+public class GameBoard {
+
+    /**
+     * Default constructor. 
+     */
+    public GameBoard() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
