0fadfc292b194270c932083239874d7431f256ee
qingfeng4119@gmail.com <qingfeng4119@gmail.com@cd6f5ea5-f733-c8f2-e759-6d7a7f71d7d3>
2010-10-27 04:33:52 +0000

    check this out on Netbeans and run.

--- aseProject/aseProject-app-client/src/java/aseproject/registerPanel.java
+++ aseProject/aseProject-app-client/src/java/aseproject/registerPanel.java
@@ -2,11 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package aseproject;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -15,33 +15,43 @@ import javax.swing.JPasswordField;
  *
  * @author Administrator
  */
-public class registerPanel extends JPanel implements ActionListener{
+public class registerPanel extends JPanel implements ActionListener {
+
     public boolean nFlag_registered = false;
     JButton newAccountBtn;
     JButton loginBtn;
     JPasswordField pswField;
+    JButton loginOKBtn;
 
     public registerPanel() {
         newAccountBtn = new JButton("Create New Account");
         loginBtn = new JButton("Log in with Existed Account");
+        loginOKBtn = new JButton("OK");
+        loginOKBtn.addActionListener(this);
         pswField = new JPasswordField(4);
+        pswField.addActionListener(this);
         add(newAccountBtn);
         add(loginBtn);
         newAccountBtn.addActionListener(this);
         loginBtn.addActionListener(this);
     }
 
-
-
-
     public void actionPerformed(ActionEvent e) {
         Object src = e.getSource();
-        if(src == newAccountBtn)
-        {
+        if (src == loginBtn) {
             add(pswField);
-            nFlag_registered=true;
+            add(loginOKBtn);
+            revalidate();
+        }
+        if (src == loginOKBtn) {
+
+            char[] input = pswField.getPassword();
+            boolean isCorrect;
+            char[] correctPassword = {'b', 'u'};
+            isCorrect = Arrays.equals(input, correctPassword);
+            if (isCorrect) {
+                nFlag_registered = true;
+            }
         }
     }
-
-
 }
