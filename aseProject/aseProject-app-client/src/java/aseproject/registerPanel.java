4854d3fe49f5e9204a3670f0c25dce5007bb79e6
talklittle <talklittle@cd6f5ea5-f733-c8f2-e759-6d7a7f71d7d3>
2010-11-06 02:58:21 +0000

    registerPanel recommit

--- aseProject/aseProject-app-client/src/java/aseproject/registerPanel.java
+++ aseProject/aseProject-app-client/src/java/aseproject/registerPanel.java
@@ -212,7 +212,7 @@ public class registerPanel extends JPanel implements ActionListener {
     private PlayerEntityFacadeRemote lookupPlayerEntityFacadeRemote() {
         try {
             Context c = new InitialContext();
-            return (PlayerEntityFacadeRemote) c.lookup("java:global/aseProject/aseProject-ejb/PlayerEntityFacade");
+            return (GameEntityFacadeRemote) c.lookup("java:global/aseProject/aseProject-ejb/GameEntityFacade");
         } catch (NamingException ne) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
             throw new RuntimeException(ne);
